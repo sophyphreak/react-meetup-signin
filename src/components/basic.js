e4afb706460d4d80ed4e6ffb759bedf6a58aee60
Andrew Horn <arhorn@smcm.edu>
2019-06-01 23:20:01 -0600

    form now functional with csv download

--- src/components/basic.js
+++ src/components/basic.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
+import moment from 'moment';
+import { CSVLink } from 'react-csv';
 
 const SignInSchema = Yup.object().shape({
   name: Yup.string()
@@ -23,64 +25,187 @@ const SignInSchema = Yup.object().shape({
     .required('Required')
 });
 
-const Basic = () => (
-  <div>
-    <Formik
-      initialValues={{
-        name: '',
-        email: '',
-        heardAboutUs: '',
-        amountPaid: '',
-        paymentMethod: ''
-      }}
-      validationSchema={SignInSchema}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          setSubmitting(false);
-        }, 400);
-      }}
-    >
-      {({ isSubmitting }) => (
-        <Form>
-          <label>Name</label>
-          <br />
-          <Field type="text" name="name" />
-          <ErrorMessage name="email" component="div" />
-          <br />
-          <br />
-          <label>Email</label>
-          <br />
-          <Field type="email" name="email" />
-          <ErrorMessage name="email" component="div" />
-          <br />
-          <br />
-          <label>How'd you hear about us</label>
-          <br />
-          <Field type="text" name="heardAboutUs" />
-          <ErrorMessage name="heardAboutUs" component="div" />
-          <br />
-          <br />
-          <label>Amount Paid</label>
-          <br />
-          <label>$</label>
-          <Field type="number" name="amountPaid" />
-          <ErrorMessage name="amountPaid" component="div" />
-          <br />
-          <br />
-          <label>Payment Method</label>
-          <br />
-          <Field type="text" name="paymentMethod" />
-          <ErrorMessage name="paymentMethod" component="div" />
-          <br />
-          <br />
-          <button type="submit" disabled={isSubmitting}>
-            Submit
-          </button>
-        </Form>
-      )}
-    </Formik>
-  </div>
-);
+const initialValues = {
+  name: '',
+  email: '',
+  heardAboutUs: '',
+  amountPaid: '',
+  paymentMethod: ''
+};
+
+class Basic extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      personList: []
+    };
+    this.addSignIn = this.addSignIn.bind(this);
+  }
+  addSignIn(person) {
+    person.date = moment().format('dddd, MMMM Do YYYY');
+    const { personList } = this.state;
+    personList.push(person);
+    this.setState(() => ({ personList }));
+  }
+  clearState() {
+    const personList = [];
+    this.setState(() => ({ personList }));
+  }
+  render() {
+    return (
+      <div>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={SignInSchema}
+          onSubmit={(values, { setSubmitting, resetForm }) => {
+            this.addSignIn(values);
+            resetForm(initialValues);
+            setSubmitting(false);
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <label>Name</label>
+              <br />
+              <Field type="text" name="name" />
+              <ErrorMessage name="email" component="div" />
+              <br />
+              <br />
+              <label>Email</label>
+              <br />
+              <Field type="email" name="email" />
+              <ErrorMessage name="email" component="div" />
+              <br />
+              <br />
+              <label>How'd you hear about us</label>
+              <br />
+              <Field type="text" name="heardAboutUs" />
+              <ErrorMessage name="heardAboutUs" component="div" />
+              <br />
+              <br />
+              <label>Amount Paid</label>
+              <br />
+              <label>$</label>
+              <Field type="number" name="amountPaid" />
+              <ErrorMessage name="amountPaid" component="div" />
+              <br />
+              <br />
+              <label>Payment Method</label>
+              <br />
+              <Field type="text" name="paymentMethod" />
+              <ErrorMessage name="paymentMethod" component="div" />
+              <br />
+              <br />
+              <button type="submit" disabled={isSubmitting}>
+                Submit
+              </button>
+            </Form>
+          )}
+        </Formik>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <CSVLink
+          data={this.state.personList}
+          headers={[
+            { label: 'Name', key: 'name' },
+            { label: 'Email', key: 'email' },
+            { label: 'Hear about us', key: 'heardAboutUs' },
+            { label: 'Amount Paid', key: 'amountPaid' },
+            { label: 'Payment Method', key: 'paymentMethod' },
+            { label: 'Date', key: 'date' }
+          ]}
+          filename={`Kadampa signin ${moment().format('dddd, MMMM Do')}`}
+        >
+          Download data
+        </CSVLink>
+        <br />
+        <br />
+        <button onClick={() => this.clearState()}>clear data</button>
+      </div>
+    );
+  }
+}
 
 export default Basic;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
-import Image from '../components/image';
 import SEO from '../components/seo';
 import Basic from '../components/basic';
 
@@ -10,7 +8,6 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Basic />
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 );
 
