8a1fcbebed6dab8232bf770a018a4ea16444695f
Andrew Horn <arhorn@smcm.edu>
2019-06-07 21:27:54 -0600

    updated seo description and author

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Kadampa Meditation Sign In`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`
+    description: `Houston Kadampa meditation group sign in sheet`,
+    author: `Andrew Horn`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
