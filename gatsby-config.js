aeafb944257dbae6e1ec2334500cbf8dc1313eef
Andrew Horn <arhorn@smcm.edu>
2019-06-09 14:02:14 -0600

    added progressive web app functionality

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,17 +18,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `kadampa-meditation-sign-in`,
+        short_name: `kadampa-sign-in`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
       }
-    }
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    },
+    `gatsby-plugin-offline`
   ]
 };
