b5c30bea90fca115d9bedf7926c45211117c1a6e
Andrew Horn <arhorn@smcm.edu>
2019-06-12 20:26:01 -0500

    Added modals functionality to clear button

--- src/components/adminButtons/clearDataButton/clearDataButton.js
+++ src/components/adminButtons/clearDataButton/clearDataButton.js
@@ -1,10 +1,55 @@
-import React from 'react';
-import { Button } from 'reactstrap';
+import React, { Component } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
-const ClearStateButton = props => (
-  <Button color="warning" onClick={() => props.clearState()}>
-    Clear data
-  </Button>
-);
+class ClearStateButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clearDataModal: false,
+      successModal: false
+    };
+
+    this.toggle = this.toggle.bind(this);
+  }
+  triggerClearState() {
+    this.toggle();
+    this.setState({ successModal: true });
+    this.props.clearState();
+    setTimeout(() => {
+      this.setState({ successModal: false });
+    }, 3000);
+  }
+  toggle() {
+    this.setState(prevState => ({
+      clearDataModal: !prevState.clearDataModal
+    }));
+  }
+  render() {
+    return (
+      <div>
+        <Button color="warning" onClick={() => this.toggle()}>
+          Clear data
+        </Button>
+        <Modal isOpen={this.state.clearDataModal} centered={true}>
+          <ModalHeader>Clear data</ModalHeader>
+          <ModalBody>
+            Do you really want to clear all data in this app?
+          </ModalBody>
+          <ModalFooter>
+            <Button color="danger" onClick={() => this.triggerClearState()}>
+              Yes, clear all data now
+            </Button>
+            <Button color="secondary" onClick={() => this.toggle()}>
+              cancel
+            </Button>
+          </ModalFooter>
+        </Modal>
+        <Modal isOpen={this.state.successModal} centered={true}>
+          <ModalBody>Success! You cleared all data!</ModalBody>
+        </Modal>
+      </div>
+    );
+  }
+}
 
 export default ClearStateButton;
