140724c44fa0d24cb4545795dcbb64c34175a645
Andrew Horn <arhorn@smcm.edu>
2019-06-18 15:41:59 -0500

    removed dead code

--- src/components/formikForm/heardAboutUsInput/heardAboutUs.js
+++ src/components/formikForm/heardAboutUsInput/heardAboutUs.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Field } from 'formik';
 import { Input, Label } from 'reactstrap';
-// import BasicInput from '../basicInput';
 
 const HeardAboutUs = () => (
   <div>
