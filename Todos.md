04eef9b61708cfc83b39789cc4c5fa60a8e95c0a
Andrew Horn <arhorn@smcm.edu>
2019-06-18 15:40:12 -0500

    added dropdown for heardOfUs

--- Todos.md
+++ Todos.md
@@ -1,12 +1,13 @@
 TODOS:
 
 - add radio buttons to payment and payment method
-- add dropdown for how you heard of us
+- add "other" specific field for heardOfUs
+- add "downloaded" flag, false until download button pushed, true on submit and on clear data. clear data button doesn't appear when flag is false
+- add contact the developer button + contact page w/ netlify
+- add list of data in state at bottom of app
+
 - add device name at end of file name
 - send w/ Gmail button at bottom
 - bold headline in xlsx doc
 - make some space at top and left and below headings for making it easier on the eyes
 - add count of number of attendees (b/c why not)
-- add "downloaded" flag, false until download button pushed, true on submit and on clear data. clear data button doesn't appear when flag is false
-- add contact the developer button + contact page w/ netlify
-- add list of data in state at bottom of app
--- src/components/formikForm/heardAboutUsInput/heardAboutUs.js
+++ src/components/formikForm/heardAboutUsInput/heardAboutUs.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import BasicInput from '../basicInput';
+import { Field } from 'formik';
+import { Input, Label } from 'reactstrap';
+// import BasicInput from '../basicInput';
 
-const HeardAboutUs = props => (
-  <BasicInput
-    componentName="heardAboutUs"
-    label="How'd you hear about us"
-    type="text"
-    {...props}
-  />
+const HeardAboutUs = () => (
+  <div>
+    <Label for="heardAboutUs">How'd you hear about us</Label>
+    <br />
+    <Input tag={Field} component="select" name="heardAboutUs">
+      <option value="">Please select</option>
+      <option value="Flyer">Flyer</option>
+      <option value="Facebook/Instagram">Facebook/Instagram</option>
+      <option value="Meetup">Meetup</option>
+      <option value="Our website">Our website</option>
+      <option value="Google search">Google search</option>
+      <option value="Friend/Family">Friend/Family</option>
+      <option value="Walk by">Walk by</option>
+      <option value="Attended KMC Houston before">
+        Attended KMC Houston before
+      </option>
+    </Input>
+  </div>
 );
 
 export default HeardAboutUs;
