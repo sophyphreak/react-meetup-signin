7d7c4768afa739811f4ecb51d75f408e5bd397b6
Andrew Horn <arhorn@smcm.edu>
2019-06-19 00:29:19 -0500

    removed commented out code

--- src/containers/signInForm/signInForm.js
+++ src/containers/signInForm/signInForm.js
@@ -77,12 +77,4 @@ class SignInForm extends Component {
   }
 }
 
-// const getLocalStorage = () => {
-//   let local = [];
-//   if (!JSON.parse(localStorage.getItem('local'))) {
-//     return local;
-//   }
-//   return JSON.parse(localStorage.getItem('local'));
-// };
-
 export default SignInForm;
