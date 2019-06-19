0c1abc6ea09d1ace2963a63949e170f9f798bd88
Andrew Horn <arhorn@smcm.edu>
2019-06-18 23:11:14 -0500

    added radio buttons input for paymentMethods

--- src/components/formikForm/paymentMethodInput/paymentMethodInput.js
+++ src/components/formikForm/paymentMethodInput/paymentMethodInput.js
@@ -1,13 +1,46 @@
 import React from 'react';
-import BasicInput from '../basicInput';
+import { Label, Input, FormGroup } from 'reactstrap';
+import { Field } from 'formik';
 
-const PaymentMethodInput = props => (
-  <BasicInput
-    componentName="paymentMethod"
-    label="Payment Method"
-    type="text"
-    {...props}
-  />
+const PaymentMethodInput = ({ value }) => (
+  <div>
+    <Label>Payment Method</Label>
+    <br />
+    <FormGroup name="amountPaid" check>
+      <Label check>
+        <Input
+          checked={value === 'prepaid'}
+          tag={Field}
+          type="radio"
+          name="paymentMethod"
+          value="prepaid"
+        />
+        prepaid
+      </Label>
+      <br />
+      <Label check>
+        <Input
+          checked={value === 'cash'}
+          tag={Field}
+          type="radio"
+          name="paymentMethod"
+          value="cash"
+        />
+        cash
+      </Label>
+      <br />
+      <Label check>
+        <Input
+          checked={value === 'credit card'}
+          tag={Field}
+          type="radio"
+          name="paymentMethod"
+          value="credit card"
+        />
+        credit card
+      </Label>
+    </FormGroup>
+  </div>
 );
 
 export default PaymentMethodInput;
