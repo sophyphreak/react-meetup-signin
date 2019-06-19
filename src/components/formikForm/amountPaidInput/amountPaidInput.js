4399dbddddadc2600a4eab88dea2dade7a4aac3c
Andrew Horn <arhorn@smcm.edu>
2019-06-18 23:10:53 -0500

    added radio buttons input for amountPaid

--- src/components/formikForm/amountPaidInput/amountPaidInput.js
+++ src/components/formikForm/amountPaidInput/amountPaidInput.js
@@ -1,13 +1,46 @@
 import React from 'react';
-import BasicInput from '../basicInput';
+import { Label, Input, FormGroup } from 'reactstrap';
+import { Field } from 'formik';
 
-const AmountPaidInput = props => (
-  <BasicInput
-    componentName="amountPaid"
-    label="Amount Paid"
-    type="text"
-    {...props}
-  />
+const AmountPaidInput = ({ value }) => (
+  <div>
+    <Label>Amount Paid</Label>
+    <br />
+    <FormGroup name="amountPaid" check>
+      <Label check>
+        <Input
+          checked={value === 'prepaid'}
+          tag={Field}
+          type="radio"
+          name="amountPaid"
+          value="prepaid"
+        />
+        prepaid
+      </Label>
+      <br />
+      <Label check>
+        <Input
+          checked={value === '$10'}
+          tag={Field}
+          type="radio"
+          name="amountPaid"
+          value="$10"
+        />
+        $10
+      </Label>
+      <br />
+      <Label check>
+        <Input
+          checked={value === '$35'}
+          tag={Field}
+          type="radio"
+          name="amountPaid"
+          value="$35"
+        />
+        $35
+      </Label>
+    </FormGroup>
+  </div>
 );
 
 export default AmountPaidInput;
