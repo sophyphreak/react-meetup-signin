1380532a01f887d9573ef259f80bfba257f0e73b
Andrew Horn <arhorn@smcm.edu>
2019-06-19 00:21:24 -0500

    data now correctly handles Other selections

--- src/containers/signInForm/signInForm.js
+++ src/containers/signInForm/signInForm.js
@@ -34,6 +34,15 @@ class SignInForm extends Component {
   addSignIn(person) {
     this.setState({ successModal: true });
     person.date = moment().format('dddd, MMMM Do YYYY');
+    if (person.heardAboutUs === 'Other' && person.other.heardAboutUs) {
+      person.heardAboutUs = person.other.heardAboutUs;
+    }
+    if (person.amountPaid === 'other' && person.other.amountPaid) {
+      person.amountPaid = person.other.amountPaid;
+    }
+    if (person.paymentMethod === 'other' && person.other.paymentMethod) {
+      person.paymentMethod = person.other.paymentMethod;
+    }
     const { personList } = this.state;
     personList.push(person);
     this.setState(() => ({ personList }));
